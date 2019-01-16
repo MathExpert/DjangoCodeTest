04adfd8bd06e965e41e6d9671dd3f5080a6d6e8b
Dmitry Mozolev <mozolevds@gmail.com>
2019-01-17 11:08:38 +1300

    Add questionnaire template

--- cloud_test_app/questionnaire/views.py
+++ cloud_test_app/questionnaire/views.py
@@ -4,6 +4,11 @@ from django.urls import reverse
 
 from . models import Favourite
 
+import calendar
+
+month_names = [calendar.month_name[i] for i in range(1, 13)]
+weekday_names = [calendar.day_name[i] for i in range(7)]
+
 def index(request):
     num_answers = Favourite.objects.count()
     context = {
@@ -13,7 +18,11 @@ def index(request):
     return render(request, 'questionnaire/index.html', context)
 
 def questionnaire(request):
-    return HttpResponse('Questionnaire view placeholder. Wait for it!')
+    context = {
+        'month_names': month_names,
+        'weekday_names': weekday_names,
+    }
+    return render(request, 'questionnaire/questionnaire.html', context)
 
 def results(request):
     return HttpResponse('Results view placeholder. More incoming!')

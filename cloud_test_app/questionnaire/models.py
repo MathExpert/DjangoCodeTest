9d660d2dacc23797a68fdbe3e3eccebf3fe7eaa0
Dmitry Mozolev <mozolevds@gmail.com>
2019-01-16 21:38:14 +1300

    Add model and migrations

--- cloud_test_app/questionnaire/models.py
+++ cloud_test_app/questionnaire/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+months = 'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec'.split(',')
+weekdays = 'Mon,Tue,Wed,Thu,Fri,Sat,Sun'.split(',')
+
+class Favourite(models.Model):
+    fav_month = models.PositiveSmallIntegerField()
+    fav_weekday = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        repr_format = 'Month: {}, Weekday: {}'
+        try:
+            return repr_format.format(months[self.fav_month], weekdays[self.fav_weekday])
+        except IndexError:
+            # We shouldn't get here if questionnaire answer data is handled correctly
+            return repr_format.format('??', '??')
+

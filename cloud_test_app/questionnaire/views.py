5dcf66e8803831cb6df2266374466fc0881af2ba
Dmitry Mozolev <mozolevds@gmail.com>
2019-01-17 13:02:28 +1300

    Handle questionnaire answer submitting

--- cloud_test_app/questionnaire/views.py
+++ cloud_test_app/questionnaire/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 from . models import Favourite
 
@@ -28,4 +29,21 @@ def results(request):
     return HttpResponse('Results view placeholder. More incoming!')
 
 def answered(request):
+    def on_bad_data(request):
+        messages.add_message(request, messages.ERROR, 'There was a problem with sent data, please answer again.')
+        return HttpResponseRedirect(reverse('questionnaire:questionnaire'))
+
+    # Some checks in case of malicious or erroneous POST request
+    try:
+        fav_month = int(request.POST['month'])
+        fav_weekday = int(request.POST['weekday'])
+    except (ValueError, KeyError):
+        return on_bad_data(request)
+
+    if fav_month < 1 or fav_month > 12 or fav_weekday < 1 or fav_weekday > 7:
+        return on_bad_data(request)
+
+    new_fav = Favourite(fav_month=fav_month, fav_weekday=fav_weekday)
+    new_fav.save()
+
     return HttpResponseRedirect(reverse('questionnaire:index'))
